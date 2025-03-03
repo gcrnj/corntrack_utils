b0e7b747b3f547477215fada90ee161ec7b0be91
Gio Cornejo - Local <giojenroc@gmail.com>
2025-03-03 15:16:25 +0800

    Added baseFirebaseStorageUrl

--- lib/utils/string_utility.dart
+++ lib/utils/string_utility.dart
@@ -1,3 +1,6 @@
+
+final String baseFirebaseStorageUrl = 'https://firebasestorage.googleapis.com/v0/b/project-corntrack.firebasestorage.app/o';
+
 String capitalizeFirstLetter(String str) {
   if (str.isEmpty) return str; // Handle empty string
   return str[0].toUpperCase() + str.substring(1);
