644de4074661802d86a307480a89b8f064b06947
Gio Cornejo - Local <giojenroc@gmail.com>
2025-03-03 17:23:49 +0800

    Edited baseFirebaseStorageUrl

--- lib/utils/string_utility.dart
+++ lib/utils/string_utility.dart
@@ -1,5 +1,5 @@
 
-String baseFirebaseStorageUrl = 'https://firebasestorage.googleapis.com/v0/b/project-corntrack.firebasestorage.app/o';
+String baseFirebaseStorageUrl = 'https://firebasestorage.googleapis.com/v0/b/project-corntrack.firebasestorage.app/o/';
 
 String capitalizeFirstLetter(String str) {
   if (str.isEmpty) return str; // Handle empty string
